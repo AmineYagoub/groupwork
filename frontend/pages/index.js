dac21ce26f99ede5200509c6149c041c2b2c10b9
Amine Yagoub <yagoub.2.amine@gmail.com>
2021-02-10 15:15:19 +0100

    update the manifest files

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.ts from my machine! Live coding</a>
+          Welcome to <a href="https://nextjs.org">Next.ts from nextjs local</a>
         </h1>
 
         <p className={styles.description}>
