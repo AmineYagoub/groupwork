80f81a54e76409e61252c2570fa68d750399f3eb
Amine Yagoub <yagoub.2.amine@gmail.com>
2021-02-10 11:48:03 +0100

    fix okteto manifest files

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.ts!</a>
+          Welcome to <a href="https://nextjs.org">Next.ts from my machine! again</a>
         </h1>
 
         <p className={styles.description}>
