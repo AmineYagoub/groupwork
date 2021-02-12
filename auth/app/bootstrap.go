5ad2cea232a73e56fbc5c3705921d9955f7b04f9
Amine Yagoub <yagoub.2.amine@gmail.com>
2021-02-12 18:49:49 +0100

    install gin lib in auth microservice

--- auth/app/bootstrap.go
+++ auth/app/bootstrap.go
@@ -1,10 +1,14 @@
 package app
 
-import (
-	"log"
-)
+import "github.com/gin-gonic/gin"
 
 // Bootstrap just boot the app
 func Bootstrap() {
-	log.Print("Hi App")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8020") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
